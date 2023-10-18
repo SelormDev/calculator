d02d41f98cc5db299ffc3a016de49d94c245b89d
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2023-10-18 19:52:38 +0000

    Completed the calculation functionality

--- js/app.js
+++ js/app.js
@@ -1,32 +1,79 @@
-const btn = document.querySelectorAll(".btn");
-const screen = document.querySelector(".screen");
-const clearBtn = document.querySelector(".clear_all");
-const divideBtn = document.querySelector(".division");
-const multiplyBtn = document.querySelector(".multiplication");
-const minusBtn = document.querySelector(".subtraction");
-const plusBtn = document.querySelector(".addition");
-const equalBtn = document.querySelector(".equals");
-const calculationSection = document.querySelector(".calculation_section");
-const total_section = document.querySelector(".total_section");
+"use strict";
 
-btn.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (button.textContent === "AC") {
-      total_section.textContent = "";
-      calculationSection.textContent = "";
-    } else if (button.textContent === "•") {
-      total_section.textContent += ".";
-    } else if (
-      button.textContent !== "Ans" &&
-      button.textContent !== "±" &&
-      button.textContent !== "x²" &&
-      button.textContent !== "x!" &&
-      button.textContent !== "." &&
-      button.textContent !== "%" &&
-      button.textContent !== "(" &&
-      button.textContent !== ")"
-    ) {
-      total_section.textContent += button.textContent;
+const num = document.querySelectorAll(".num");
+const operators = document.querySelectorAll(".operators");
+const clearBtn = document.querySelector(".clear_all");
+let calculationSection = document.querySelector(".calculation_section");
+let totalSection = document.querySelector(".total_section");
+let displayValue = 0;
+
+let firstNumber;
+let sign;
+let secondNumber;
+
+const add = (firstNumber, secondNumber) => {
+  return firstNumber + secondNumber;
+};
+
+const subtract = (firstNumber, secondNumber) => {
+  return firstNumber - secondNumber;
+};
+
+const multiply = (firstNumber, secondNumber) => {
+  return firstNumber * secondNumber;
+};
+
+const divide = (firstNumber, secondNumber) => {
+  return firstNumber / secondNumber;
+};
+const calculate = (operator, firstNumber, secondNumber) => {
+  if (operator === "+") {
+    return add(firstNumber, secondNumber);
+  } else if (operator === "−") {
+    return subtract(firstNumber, secondNumber);
+  } else if (operator === "×") {
+    return multiply(firstNumber, secondNumber);
+  } else if (operator === "÷") {
+    return divide(firstNumber, secondNumber);
+  } else {
+    return secondNumber;
+  }
+};
+
+function updateDisplayValue() {
+  totalSection.textContent += this.textContent;
+  displayValue = Number(totalSection.textContent);
+}
+
+for (let i = 0; i < num.length; i++) {
+  num[i].addEventListener("click", updateDisplayValue);
+}
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    switch (operator.textContent) {
+      case "+":
+      case "−":
+      case "×":
+      case "÷":
+        if (calculationSection.textContent !== "") {
+          secondNumber = displayValue;
+          firstNumber = calculate(sign, firstNumber, secondNumber);
+          totalSection.textContent = firstNumber;
+        } else {
+          firstNumber = displayValue;
+        }
+
+        sign = operator.textContent;
+        calculationSection.textContent = `${firstNumber} ${sign}`;
+        totalSection.textContent = "";
+        break;
     }
   });
+});
+
+clearBtn.addEventListener("click", () => {
+  calculationSection.textContent = "";
+  totalSection.textContent = "";
+  displayValue = 0;
 });
