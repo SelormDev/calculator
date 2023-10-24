53cf4c2663267c4c69db14ca712e469d21e80115
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2023-10-24 23:41:03 +0000

    Implemented equal button and fixed calculation bug

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 "use strict";
 
-const num = document.querySelectorAll(".num");
+const numbersBtn = document.querySelectorAll(".num");
 const operators = document.querySelectorAll(".operators");
 const clearBtn = document.querySelector(".clear_all");
+const equalBtn = document.querySelector(".equals");
 let calculationSection = document.querySelector(".calculation_section");
 let totalSection = document.querySelector(".total_section");
+
 let displayValue = 0;
+
+let answer = 0;
 
 let firstNumber;
 let sign;
@@ -27,16 +31,15 @@ const divide = (firstNumber, secondNumber) => {
   return firstNumber / secondNumber;
 };
 const calculate = (operator, firstNumber, secondNumber) => {
-  if (operator === "+") {
-    return add(firstNumber, secondNumber);
-  } else if (operator === "−") {
-    return subtract(firstNumber, secondNumber);
-  } else if (operator === "×") {
-    return multiply(firstNumber, secondNumber);
-  } else if (operator === "÷") {
-    return divide(firstNumber, secondNumber);
-  } else {
-    return secondNumber;
+  switch (operator) {
+    case "+":
+      return add(firstNumber, secondNumber);
+    case "−":
+      return subtract(firstNumber, secondNumber);
+    case "×":
+      return multiply(firstNumber, secondNumber);
+    case "÷":
+      return divide(firstNumber, secondNumber);
   }
 };
 
@@ -45,10 +48,11 @@ function updateDisplayValue() {
   displayValue = Number(totalSection.textContent);
 }
 
-for (let i = 0; i < num.length; i++) {
-  num[i].addEventListener("click", updateDisplayValue);
+for (let i = 0; i < numbersBtn.length; i++) {
+  numbersBtn[i].addEventListener("click", updateDisplayValue);
 }
 
+let operatorValue = true;
 operators.forEach((operator) => {
   operator.addEventListener("click", () => {
     switch (operator.textContent) {
@@ -56,24 +60,36 @@ operators.forEach((operator) => {
       case "−":
       case "×":
       case "÷":
-        if (calculationSection.textContent !== "") {
+        if (answer !== 0) {
+          firstNumber = answer;
+        } else if (calculationSection.textContent !== "") {
           secondNumber = displayValue;
           firstNumber = calculate(sign, firstNumber, secondNumber);
-          totalSection.textContent = firstNumber;
         } else {
           firstNumber = displayValue;
         }
 
         sign = operator.textContent;
-        calculationSection.textContent = `${firstNumber} ${sign}`;
+        calculationSection.textContent = `${firstNumber}${sign}`;
         totalSection.textContent = "";
         break;
     }
   });
 });
 
-clearBtn.addEventListener("click", () => {
+function reset() {
   calculationSection.textContent = "";
   totalSection.textContent = "";
   displayValue = 0;
+  answer = 0;
+}
+
+equalBtn.addEventListener("click", () => {
+  if (calculationSection.textContent) {
+    answer = calculate(sign, firstNumber, displayValue);
+    totalSection.textContent = answer;
+    calculationSection.textContent = "";
+  }
 });
+
+clearBtn.addEventListener("click", reset);
